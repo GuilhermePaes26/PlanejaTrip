c5516234de4f65cd41f080fba2b2e57da06ed255
unknown <guilherme_jaeke@hotmail.com>
2025-04-16 22:46:16 -0300

    Erro no formGroup ainda não solucionado

--- FrontEnd-viajatcc/src/app/components/auth/login/login.component.ts
+++ FrontEnd-viajatcc/src/app/components/auth/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';  // Importe as classes necessárias
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  logForm!: FormGroup;  // Defina a variável do FormGroup
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // Inicialize o formulário com os controles e validadores
+    this.logForm = this.fb.group({
+      username: ['', [Validators.required]],  // Adicionando validadores
+      password: ['', [Validators.required]]   // Adicionando validadores
+    });
+  }
+
+  // Função que será chamada no submit do formulário
+  onLogin() {
+    if (this.logForm.valid) {
+      const { username, password } = this.logForm.value;
+      // Faça algo com os dados, como chamar um serviço de login, por exemplo
+      console.log('Login com:', username, password);
+    } else {
+      this.errorMessage = 'Preencha todos os campos corretamente!';
+    }
+  }
 }
