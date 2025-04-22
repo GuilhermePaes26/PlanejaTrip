8e6cb4ee09a8efd78a28835a37460a6258d25fad
GuiJaeke <guilherme_jaeke@hotmail.com>
2025-04-21 23:11:25 -0300

    Criando service para usuário e resgatando dados do usuário no template

--- viajatcc-frontend/src/app/components/home/dashboard/dashboard.component.ts
+++ viajatcc-frontend/src/app/components/home/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { user, UserService } from '../../../services/user.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  user!: user
+  token: string | null = ''
+  constructor(private userService: UserService, private authService: AuthService) {
+    this.token = this.authService.getToken()
+    this.userService.getUser(this.token).subscribe({
+      next: (response) => {
+        this.user = response
+        console.log(response);
+      }
+    })
+  }
 }
