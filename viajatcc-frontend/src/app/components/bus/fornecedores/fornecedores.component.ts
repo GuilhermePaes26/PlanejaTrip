d0ef69c835733427fabce394f10adabf6775a1b5
GuiJaeke <guilherme_jaeke@hotmail.com>
2025-04-21 22:52:44 -0300

    Adicionando botão em toda div para acessar apenas um fornecedor

--- viajatcc-frontend/src/app/components/bus/fornecedores/fornecedores.component.ts
+++ viajatcc-frontend/src/app/components/bus/fornecedores/fornecedores.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BusService, supplier } from '../../../services/bus.service';
 
 @Component({
   selector: 'app-fornecedores',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './fornecedores.component.scss'
 })
 export class FornecedoresComponent {
-
+  suppliers: supplier[] = []
+  isLoading = true;
+  error = '';
+  constructor(private busService: BusService ) {
+    this.busService.getSuppliers().subscribe({
+      next: (response) => {
+        this.suppliers = response
+        this.isLoading = false;
+    },
+      error: (err) => {
+        this.error = 'Erro ao carregar os fornecedores';
+        this.isLoading = false;
+        console.error(err);
+      }
+  })
+  }
+  viewSupplier(id: string) {
+    console.log(id);
+    
+  }
 }
--- viajatcc-frontend/src/app/services/bus.service.ts
+++ viajatcc-frontend/src/app/services/bus.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface supplier {
+  _id?: string;
+  nome: string;
+  endereco: string;
+  telefone: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusService {
-
-  constructor() { }
+  private readonly apiUrl = 'http://localhost:3000/suppliers';
+  constructor(private http: HttpClient) { }
+  getSuppliers() {
+    return this.http.get<supplier[]>(this.apiUrl)
+  }
 }
